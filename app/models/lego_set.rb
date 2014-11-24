3e2059736786ea263a50ab76193d03a933588fa6
Taylor Mock <tmock12@gmail.com>
2014-11-23 21:46:56 -0500

    Add 'out of stock' to LegoStockChecker

--- app/models/lego_set.rb
+++ app/models/lego_set.rb
@@ -3,9 +3,9 @@ class LegoSet < ActiveRecord::Base
   after_create :check_stock!
 
   def check_stock!
-    self.walmart_in_stock = WalmartStockChecker.in_stock?(walmart_url) if walmart_url
-    self.amazon_in_stock = AmazonStockChecker.in_stock?(amazon_url) if amazon_url
-    self.lego_in_stock = LegoStockChecker.in_stock?(lego_url) if lego_url
+    self.walmart_in_stock = WalmartStockChecker.in_stock?(walmart_url) if walmart_url.present?
+    self.amazon_in_stock = AmazonStockChecker.in_stock?(amazon_url) if amazon_url.present?
+    self.lego_in_stock = LegoStockChecker.in_stock?(lego_url) if lego_url.present?
     self.save
   end
 end
--- app/stock_checkers/lego_stock_checker.rb
+++ app/stock_checkers/lego_stock_checker.rb
@@ -1,5 +1,5 @@
 class LegoStockChecker < BaseStockChecker
   def in_stock?
-    !page.at_css("li[class*='availability']").text.include?('Sold Out')
+    !page.at_css("li[class*='availability']").text.match(/sold out|out of stock/i)
   end
 end
