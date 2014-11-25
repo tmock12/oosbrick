292957559c9579e49f5c3f305363f328cf3130c4
Taylor Mock <tmock12@gmail.com>
2014-11-25 08:12:49 -0500

    Fix OOS bug for TRU

--- app/stock_checkers/tru_stock_checker.rb
+++ app/stock_checkers/tru_stock_checker.rb
@@ -1,5 +1,5 @@
 class TruStockChecker < BaseStockChecker
   def in_stock?
-    !page.at_css("li.unavail") && !!page.at_css("li.avail")
+    !page.at_css("#eligibility:first-child li.unavail") && !!page.at_css("li.avail")
   end
 end
