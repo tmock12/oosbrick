dcf8b5edb5cbc566566c8401e90174c9389f8992
Taylor Mock <tmock12@gmail.com>
2014-11-26 22:35:41 -0500

    Target and Walmart require pre-processing
    This reverts commit 2a3cb0d4c45c6addb8f3f6639ec65076a4c171e9.

--- app/stock_checkers/walmart_stock_checker.rb
+++ app/stock_checkers/walmart_stock_checker.rb
@@ -2,4 +2,8 @@ class WalmartStockChecker < BaseStockChecker
   def in_stock?
     !page.at_css('.btn-out-of-stock') && !page.at_css('.outofStockMsg')
   end
+
+  def needs_preprocessing?
+    true
+  end
 end
