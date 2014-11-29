92d70e2623d644bf4eb53cf6d6a4e9f31ac9dc58
Taylor Mock <tmock12@gmail.com>
2014-11-28 22:09:32 -0500

    Move stock checkers to external gem

--- app/models/lego_set.rb
+++ app/models/lego_set.rb
@@ -17,11 +17,11 @@ class LegoSet < ActiveRecord::Base
 
   def stock_hash
     {
-      walmart_in_stock: WalmartStockChecker.in_stock?(walmart_url),
-      amazon_in_stock: AmazonStockChecker.in_stock?(amazon_url),
-      lego_in_stock: LegoStockChecker.in_stock?(lego_url),
-      tru_in_stock: TruStockChecker.in_stock?(tru_url),
-      target_in_stock: TargetStockChecker.in_stock?(target_url)
+      walmart_in_stock: LegoStockChecker::WalmartCheck.in_stock?(walmart_url),
+      amazon_in_stock: LegoStockChecker::AmazonCheck.in_stock?(amazon_url),
+      lego_in_stock: LegoStockChecker::LegoShopCheck.in_stock?(lego_url),
+      tru_in_stock: LegoStockChecker::TruCheck.in_stock?(tru_url),
+      target_in_stock: LegoStockChecker::TargetCheck.in_stock?(target_url)
     }
   end
 end
--- app/stock_checkers/amazon_stock_checker.rb
+++
@@ -1,5 +0,0 @@
-class AmazonStockChecker < BaseStockChecker
-  def in_stock?
-    page.at_css('#merchant-info').text.include?('Ships from and sold by Amazon.com')
-  end
-end
